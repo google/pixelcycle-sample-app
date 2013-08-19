aeff110ea01b37abe2fb5994ce7525a788f8a7d7
Brian Slesinsky <brian-git@slesinsky.org>
2013-08-18 22:55:53 -0700

    got StripView widget working with touch

--- lib/src/ui.dart
+++ lib/src/ui.dart
@@ -2,25 +2,24 @@ library ui;
 
 import 'dart:async' show StreamSubscription;
 import 'dart:html';
-import 'dart:math';
 
 import 'package:pixelcycle2/src/movie.dart' show WIDTH, HEIGHT, LARGE, ALL, Movie, Frame, Size;
 import 'package:pixelcycle2/src/player.dart' show Player;
 
 void onLoad(Player player) {
   Movie movie = player.movie;
-  
+
   for (CanvasElement elt in queryAll('canvas[class="frameview"]')) {
     var size = new Size(elt.attributes["data-size"]);
     var f = new FrameView(elt, size, movie);
     player.onTimeChange.listen((num time) {
-      f.render(player.positionAt(time));      
+      f.render(player.positionAt(time));
     });
   }
-  
+
   for (CanvasElement elt in queryAll('canvas[class="stripview"]')) {
     var size = new Size(elt.attributes["data-size"]);
-    var strip = new StripView(elt, size, player);    
+    var strip = new StripView(elt, size, player);
     player.onTimeChange.listen((num time) {
       strip.render(player.positionAt(time));
     });
@@ -36,9 +35,9 @@ class FrameView {
     elt.width = WIDTH * size.pixelsize;
     elt.height = HEIGHT * size.pixelsize;
   }
-  
-  void render(num moviePosition) { 
-    var frame = movie.frames[moviePosition ~/ 1];      
+
+  void render(num moviePosition) {
+    var frame = movie.frames[moviePosition ~/ 1];
     frame.render(elt.context2D, size, ALL);
   }
 }
@@ -50,16 +49,21 @@ class StripView {
   final Size size;
   final Player player;
   final int height = HEIGHT + SPACER;
-  
+
   StreamSubscription moveSub;
   num lastTime;
   num lastY;
-  
+
+  var touchId = null;
+  StreamSubscription touchMoveSub;
+  num lastTouchTime;
+  num lastTouchY;
+
   StripView(this.elt, this.size, this.player) {
     elt.width = WIDTH + SPACER * 2;
     elt.height = HEIGHT * LARGE.pixelsize;
     elt.style.backgroundColor = "#000000";
-    
+
     elt.onMouseDown.listen((e) {
       e.preventDefault();
       player.playing = false;
@@ -70,10 +74,35 @@ class StripView {
         moveSub = elt.onMouseMove.listen(drag);
       }
     });
-    
-    elt.onMouseUp.listen((e) => stopDragging());   
-    elt.onMouseOut.listen((e) => stopDragging());      
+
+    elt.onMouseUp.listen((e) => stopDragging());
+    elt.onMouseOut.listen((e) => stopDragging());
     query("body").onMouseUp.listen((e) => stopDragging());
+
+    elt.onTouchStart.listen((TouchEvent e) {
+      print("onTouchStart");
+      e.preventDefault();
+      if (touchId != null) {
+        return; // ignore touches after the first
+      }
+      Touch t = e.changedTouches[0];
+      player.playing = false;
+      player.velocity = 0;
+      if (touchMoveSub == null) {
+        touchId = t.identifier;
+        lastTouchTime = window.performance.now() / 1000.0;
+        lastTouchY = t.page.y;
+        print("lastTouchY: ${lastTouchY}");
+        touchMoveSub = elt.onTouchMove.listen(touchDrag);
+      }
+    });
+
+    elt.onTouchEnd.listen((TouchEvent e) {
+      if (e.touches.isEmpty) {
+        print("onTouchEnd empty");
+        stopDragging();
+      }
+    });
   }
 
   void drag(MouseEvent e) {
@@ -85,15 +114,34 @@ class StripView {
     lastTime = now;
     lastY = e.client.y;
   }
-  
+
+  void touchDrag(TouchEvent e) {
+    for (Touch t in e.changedTouches) {
+      if (t.identifier == touchId) {
+        num now = window.performance.now() / 1000.0;
+        num deltaY = t.page.y - lastTouchY;
+        num deltaPos = -deltaY / height;
+        num deltaT = now - lastTouchTime;
+        player.drag(deltaPos, deltaT);
+        lastTouchTime = now;
+        lastTouchY = t.page.y;
+      }
+    }
+  }
+
   void stopDragging() {
     player.playing = true;
     if (moveSub != null) {
       moveSub.cancel();
       moveSub = null;
-    }    
+    }
+    if (touchMoveSub != null) {
+      touchMoveSub.cancel();
+      touchMoveSub = null;
+    }
+    touchId = null;
   }
-  
+
   void render(num moviePosition) {
     var movie = player.movie;
     elt.width = elt.width;
@@ -101,7 +149,7 @@ class StripView {
 
     int currentFrame = moviePosition ~/ 1;
     int currentFrameY = elt.height ~/ 2;
-    
+
     num startPos = (moviePosition - currentFrameY / height) % movie.frames.length;
     int frame = startPos ~/ 1;
     int frameY = ((frame - startPos) * height) ~/ 1 + SPACER ~/ 2;
@@ -109,7 +157,7 @@ class StripView {
       var peakDist = (frameY - currentFrameY).abs() / elt.height;
       c.globalAlpha = 0.6 - peakDist / 2;
       movie.frames[frame].renderAt(c, size, SPACER, frameY);
-      
+
       frame = (frame + 1) % movie.frames.length;
       frameY += height;
     }
@@ -121,3 +169,4 @@ class StripView {
     c.stroke();
   }
 }
+
