5f8133f715881f91044a417229a46c0d78364435
Brian Slesinsky <brian-git@slesinsky.org>
2013-09-01 22:19:34 -0700

    scale up image

--- web/server/gif.go
+++ web/server/gif.go
@@ -13,6 +13,8 @@ import (
 	"appengine"
 )
 
+const pixelsize = 6
+
 func init() {
 	http.HandleFunc("/gif", gifHandler)
 }
@@ -32,23 +34,36 @@ func gifHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bounds := image.Rect(0, 0, m.Width, m.Height)
-	palette := color.Palette {}
-	for i := 0; i < len(m.Palette); i+=3 {
+	palette := color.Palette{}
+	for i := 0; i < len(m.Palette); i += 3 {
 		palette = append(palette, color.RGBA{m.Palette[i], m.Palette[i+1], m.Palette[i+2], 255})
 	}
 	delay := int(100 / m.Speed)
 
-	anim := gif.GIF {LoopCount: 0}
-
+	anim := gif.GIF{LoopCount: 0}
 	for _, data := range m.Frames {
 		var pix []byte
 		json.Unmarshal([]byte(data), &pix)
-		
+
+		bounds := image.Rect(0, 0, m.Width*pixelsize, m.Height*pixelsize)
+		scaled := make([]byte, bounds.Max.X*bounds.Max.Y)
+		idx := 0
+		for y := 0; y < m.Height; y++ {
+			for i := 0; i < pixelsize; i++ {
+				for x := 0; x < m.Width; x++ {
+					pixel := pix[x+y*m.Width]
+					for j := 0; j < pixelsize; j++ {
+						scaled[idx] = pixel
+						idx++
+					}
+				}
+			}
+		}
+
 		img := image.Paletted{
-			Pix: pix,
-			Stride: m.Width,
-			Rect: bounds,
+			Pix:     scaled,
+			Stride:  m.Width * pixelsize,
+			Rect:    bounds,
 			Palette: palette,
 		}
 
